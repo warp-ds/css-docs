58c6e42da3bde62e7a6be0e3aca78e1b54e22ab5
Dave Honneffer <pearofducks@gmail.com>
2023-02-08 18:12:19 +0100

    Stop using presetUno, use preset-docs

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,13 +1,15 @@
 import uno from 'unocss/vite'
-import { presetUno, defineConfig } from 'unocss'
+import { defineConfig } from 'unocss'
 import { presetWarp } from '@warp-ds/uno'
+import { presetDocs } from '@warp-ds/preset-docs'
 
 export default {
   plugins: [
     uno(defineConfig({
       presets: [
-        presetWarp({ usePixels: true }),
-        presetUno()
+        presetWarp({ usePixels: true, usePreflight: false }),
+        presetDocs()
+        // presetUno()
       ]
     }))
   ]
