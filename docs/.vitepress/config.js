8e8133e61cea28477fbcd026fd2c646d3908fad4
Snorre Nygaard <snorren.nygaard@finn.no>
2023-02-10 16:35:20 +0100

    Add docs for the rest of the grid and flex stuff

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -27,7 +27,25 @@ export default defineConfig({
             { text: 'Flex Direction', link: '/flex-direction/' },
             { text: 'Flex Grow', link: '/flex-grow/' },
             { text: 'Flex Shrink', link: '/flex-shrink/' },
-            { text: 'Flex Wrap', link: '/flex-wrap/' }
+            { text: 'Flex Wrap', link: '/flex-wrap/' },
+            { text: 'Order', link: '/order/' },
+            { text: 'Grid Template Columns', link: '/grid-template-columns/' },
+            { text: 'Grid Column Start / End', link: '/grid-column-se/' },
+            { text: 'Grid Template Rows', link: '/grid-template-rows/' },
+            { text: 'Grid Row Start / End', link: '/grid-row-se/' },
+            { text: 'Grid Auto Flow', link: '/grid-auto-flow/' },
+            { text: 'Grid Auto Columns', link: '/grid-auto-columns/' },
+            { text: 'Grid Auto Rows', link: '/grid-auto-rows/' },
+            { text: 'Gap', link: '/gap/' },
+            { text: 'Justify Content', link: '/justify-content/' },
+            { text: 'Justify Items', link: '/justify-items/' },
+            { text: 'Justify Self', link: '/justify-self/' },
+            { text: 'Align Content', link: '/align-content/' },
+            { text: 'Align Items', link: '/align-items/' },
+            { text: 'Align Self', link: '/align-self/' },
+            { text: 'Place Content', link: '/place-content/' },
+            { text: 'Place Items', link: '/place-items/' },
+            { text: 'Place Self', link: '/place-self/' }
           ]
         }
       ]
