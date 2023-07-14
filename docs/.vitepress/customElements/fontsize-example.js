5bcf6e973a5dd078cb3f1869df203d3bb72fc6c1
Silje Christensen <silje.christensen@cognite.com>
2023-07-14 10:59:45 +0200

    fix: fontsize example

--- docs/.vitepress/customElements/fontsize-example.js
+++ docs/.vitepress/customElements/fontsize-example.js
@@ -8,21 +8,21 @@ class FontSizeExample extends HTMLElement {
         @unocss-placeholder
       </style>
       <div class="rounded-8 bg bg-color p-24">
-        <span class="text-s">text-xs</span>
+        <span class="text-xs">text-xs</span>
         <p class="text-xs mt-0!">The quick warp scientist</p>
         <span class="text-s">text-s</span>
         <p class="text-s mt-0!">The quick warp scientist</p>
-        <span class="text-s">text-m</span>
+        <span class="text-m">text-m</span>
         <p class="text-m mt-0!">The quick warp scientist</p>
-        <span class="text-s">text-ml</span>
+        <span class="text-ml">text-ml</span>
         <p class="text-ml mt-0!">The quick warp scientist</p>
-        <span class="text-s">text-l</span>
+        <span class="text-l">text-l</span>
         <p class="text-l mt-0!">The quick warp scientist</p>
-        <span class="text-s">text-xl</span>
+        <span class="text-xl">text-xl</span>
         <p class="text-xl mt-0!">The quick warp scientist</p>
-        <span class="text-s">text-xxl</span>
+        <span class="text-xxl">text-xxl</span>
         <p class="text-xxl mt-0!">The quick warp scientist</p>
-        <span class="text-s">text-xxxl</span>
+        <span class="text-xxxl">text-xxxl</span>
         <p class="text-xxxl mt-0!">The quick warp scientist</p>
       </div>
     `;
