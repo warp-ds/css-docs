91901381baefcb5e1be97e1acf0fbc8f45526268
BalbinaK <balbuhhha@gmail.com>
2023-09-12 10:26:06 +0200

    Fix text background and add icons

--- docs/global-components/qr-color-table.vue
+++ docs/global-components/qr-color-table.vue
@@ -3,6 +3,7 @@ import { computed } from 'vue'
 import { useData } from 'vitepress'
 import { data } from '../classes.data.js'
 import camelcase from 'camelcase'
+import { IconSearch16 } from '@warp-ds/icons/vue'
 
 const camelcaseify = str => camelcase(str.replace(/[^\w\s]/gi, ''))
 const pageData = useData()
@@ -26,8 +27,9 @@ const rows = computed(() => props.list ?? data[dataTitle.value])
         <td v-if="desc === null" colspan="2">Unsupported</td>
         <template v-else>
           <td>
-            <div :class="[cls, { 's-bg-primary': /^s-text-inverted/.test(cls) },{ 'border': !/^s-bg/.test(cls) }, { 'h-24': !/^s-text/.test(cls) } ]" class="w-64 px-8">
+            <div :class="[cls, { 's-bg-inverted': /^s-(text|icon)-(inverted)/.test(cls) }, { 's-bg-notification': /^s-text-notification/.test(cls) }, { 'border': !/^s-bg/.test(cls) }, { 'h-24': !/^s-(text|icon)/.test(cls) } ]" class="w-64 px-8 text-center">
               <span v-if="/^s-text/.test(cls)">Text</span>
+              <icon-search16 v-if="/^s-icon/.test(cls)" class="m-auto my-4"></icon-search16>
             </div>
           </td>
           <td>
