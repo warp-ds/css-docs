0dac4cb6cf6522347f5d7bbdd023a2c6adc236c7
Balbina K <41303231+BalbinaK@users.noreply.github.com>
2023-09-12 11:06:03 +0200

    Update docs/global-components/qr-color-table.vue
    Co-authored-by: Magnus Hübner <magnuh@users.noreply.github.com>

--- docs/global-components/qr-color-table.vue
+++ docs/global-components/qr-color-table.vue
@@ -27,7 +27,7 @@ const rows = computed(() => props.list ?? data[dataTitle.value])
         <td v-if="desc === null" colspan="2">Unsupported</td>
         <template v-else>
           <td>
-            <div :class="[cls, { 's-bg-inverted': /^s-(text|icon)-(inverted)/.test(cls) }, { 's-bg-notification': /^s-text-notification/.test(cls) }, { 'border': !/^s-bg/.test(cls) }, { 'h-24': !/^s-(text|icon)/.test(cls) } ]" class="w-64 px-8 text-center">
+            <div :class="[cls, { 's-bg-inverted': /^s-(text|icon)-inverted/.test(cls) }, { 's-bg-notification': /^s-text-notification/.test(cls) }, { 'border': !/^s-bg/.test(cls) }, { 'h-24': !/^s-(text|icon)/.test(cls) } ]" class="w-64 px-8 text-center">
               <span v-if="/^s-text/.test(cls)">Text</span>
               <icon-search16 v-if="/^s-icon/.test(cls)" class="m-auto my-4"></icon-search16>
             </div>
