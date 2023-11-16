b2478f60edc9fbb5143ef11076115097fcb27e2e
Magnus Hübner <magnus.hubner@aptitud.se>
2023-11-16 09:48:19 +0100

    fix: Fix styling of theme switcher (and add label)

--- docs/global-components/ThemeContainer.vue
+++ docs/global-components/ThemeContainer.vue
@@ -20,49 +20,63 @@ const selectedTheme = computed({
 
 <template>
   <ClientOnly>
+    <div class="theme-switcher">
+      <label for="themeSelect">Select theme:</label>
       <div class="select">
-          <select v-model="selectedTheme" name="themes">
+        <select id="themeSelect" v-model="selectedTheme">
           <option v-for="[name, value] in Object.entries(themes)" :key="name" :value="value">{{ name }}</option>
-          </select>
+        </select>
       </div>
-      <slot />
-    </ClientOnly>
+    </div>
+    <slot />
+  </ClientOnly>
 </template>
 
 <style lang="scss" scoped>
+.theme-switcher {
+  display: flex;
+  align-items: center;
+  margin-bottom: 16px;
+  padding: 16px;
+  border: 1px solid var(--vp-c-border);
+  border-radius: 8px;
+  background-color: var(--vp-c-bg-soft);
+}
 .select {
   display: grid;
   grid-template-areas: "select";
   align-items: center;
   position: relative;
   width: 300px;
+  margin-left: 8px;
   margin-bottom: 24px;
-  padding: 4px 8px;
+  padding: 3px 8px;
+  background-color: var(--vp-c-bg);
   border: 1px solid var(--vp-c-text-2);
   border-radius: 5px;
-  background-color: var(--vp-c-bg);
 
   &:last-child {
     margin-bottom: 0;
   }
 
-  select,
-  &::after {
+  select {
     grid-area: select;
+    background-color: transparent;
   }
 
   &::after {
+    grid-area: select;
     content: "";
     justify-self: end;
     width: 8px;
     height: 5px;
-    background-color: var(--vp-code-block-bg);
+    background-color: var(--vp-c-text-1);
     clip-path: polygon(100% 0%, 0 0, 50% 100%);
   }
 
   &:focus-within {
-    border: 2px solid var(--vp-c-brand);
-    padding: 3px 7px;
+    border: 2px solid var(--vp-c-brand-1);
+    padding: 2px 7px;
   }
 }
 </style>
