23258140ebfcb7576e75f201336aa8b377f9ee15
Magnus Hübner <magnus.hubner@aptitud.se>
2024-01-11 16:10:41 +0100

    Update the color table component to handle semantic divide classes (plus misc visual improvements)

--- docs/global-components/qr-color-table.vue
+++ docs/global-components/qr-color-table.vue
@@ -1,16 +1,15 @@
 <script setup>
-import { computed } from 'vue'
-import { useData } from 'vitepress'
-import { data } from '../classes.data.js'
-import camelcase from 'camelcase'
-import { IconRatingFull16 } from '@warp-ds/icons/vue'
+import { computed } from 'vue';
+import { useData } from 'vitepress';
+import { data } from '../classes.data.js';
+import camelcase from 'camelcase';
+import { IconRatingFull16 } from '@warp-ds/icons/vue';
 
-const camelcaseify = str => camelcase(str.replace(/[^\w\s]/gi, ''))
-const pageData = useData()
-const props = defineProps({ list: Array })
-const dataTitle = computed(() => camelcaseify(pageData.page.value.title))
-const rows = computed(() => props.list ?? data[dataTitle.value])
-const outlineClasses = 'outline outline-offset-1 outline-2'
+const pageData = useData();
+const props = defineProps({ list: Array });
+const dataTitle = computed(() => camelcase(pageData.page.value.title.replace(/[^\w\s]/gi, '')));
+const rows = computed(() => props.list ?? data[dataTitle.value]);
+const outlineClasses = 'outline outline-offset-1 outline-2 border';
 </script>
 
 <template>
@@ -24,19 +23,22 @@ const outlineClasses = 'outline outline-offset-1 outline-2'
     </thead>
     <tbody>
       <tr v-for="[cls, desc] in rows">
-        <td><code>{{ cls }}</code></td>
+        <td><code :class="{'line-through': desc === null}">{{ cls }}</code></td>
         <td v-if="desc === null" colspan="2">Unsupported</td>
         <template v-else>
-          <td :class="[/^s-(text|icon|border|outline)-inverted/.test(cls) || /^s-bg$/.test(cls) ? 's-bg-inverted' : 's-bg', { 's-bg-notification': /^s-text-notification/.test(cls) }]">
-            <div :class="[cls, { 'border-2': /^(s-)?(border|outline)/.test(cls) }, { 'h-24': !/^(s-)?(text|icon)/.test(cls) }, { [outlineClasses]: /^(s-)?outline/.test(cls) } ]" class="w-64 px-8 text-center">
-              <span v-if="/^(s-text|text-)/.test(cls)" class="text-l">Text</span>
+          <td :class="[/^s-(text|icon|border|outline|divide)-inverted/.test(cls) || /^s-bg$/.test(cls) ? 's-bg-inverted s-text-inverted' : /^(s-)?bg-/.test(cls) ? 's-bg' : '', { 's-bg-notification': /^s-text-notification/.test(cls) }]">
+            <div v-if="/^(s-)?divide/.test(cls)" class="flex justify-center divide-x-4" :class="cls">
+              <div class="text-right pr-6">A</div>
+              <div class="pl-6">B</div>
+            </div>
+            <div v-else :class="[cls, { 'border-2': /^(s-)?border/.test(cls) }, { 'h-24': !/^(s-)?(text|icon)/.test(cls) }, { [outlineClasses]: /^(s-)?outline/.test(cls) } ]" class="w-64 px-8 text-center rounded">
               <icon-rating-full-16 v-if="/^s-icon/.test(cls)" class="m-auto my-4" />
+              <span v-else-if="/^(s-)?text/.test(cls)" class="text-l">Text</span>
             </div>
           </td>
           <td>
-            <code v-for="(l, i) in desc.split('\n')">
-              {{ l }}
-              <br v-if="desc.split('\n').length > 1 && i < desc.split('n').length - 1" />
+            <code class="whitespace-pre-line">
+              {{ desc }}
             </code>
           </td>
         </template>
